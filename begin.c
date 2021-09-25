07f4c80e0bb2caa71b3aa1cc96435c338043e799
joaopdrb <90069204+joaopdrb@users.noreply.github.com>
2021-09-25 15:48:48 -0300

    Update begin.c

--- begin.c
+++ begin.c
@@ -4,6 +4,6 @@ int main(void)
 {
   char *name = malloc(10 * sizeof(char));  
   printf("Enter your name: ");
-  scanf("%c\n", name);
-  printf("Hello, %c\n" name);
+  scanf("%s\n", name);
+  printf("Hello, %s\n", name);
 }
